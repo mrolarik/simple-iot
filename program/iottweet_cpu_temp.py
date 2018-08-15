b13c992d97b6f99503751c6f4a0f249b4e59a478
olarik surinta <35139872+mrolarik@users.noreply.github.com>
2018-08-15 13:05:51 +0700

    Add files via upload

---
+++ program/iottweet_cpu_temp.py
@@ -0,0 +1,42 @@
+from IoTtweet import *
+import time
+import os
+
+version = getversion()
+print(version)
+
+userid = '003061'
+key = 'lymgo0j8uwe1'
+
+f = os.popen("sensors | grep Core")
+lines = tuple(f)
+
+tw_msg = "Number of Core: " + str(len(lines)) + " cores"
+#print tw
+
+max_temp = 60
+
+slot = [0,0,0,0]
+
+while(True):
+  tw = tw_msg
+
+  for i in range(0, len(lines)):
+    print("core", i, lines[i].split('+')[1].split('.')[0])
+    slot[i] = lines[i].split('+')[1].split('.')[0]
+
+  slot_int = map(int, slot)
+  temp_avg = sum(slot_int) / float(len(slot_int))
+  print temp_avg
+
+  if(temp_avg >= max_temp):
+    twpb = 'CPU temp (' + str(temp_avg)  + 'c) is overhead!!!'
+  else:
+    twpb = 'CPU temp (' + str(temp_avg)  + 'c) is normal'
+
+  #Send data to IoTtweet dashboard.
+  res = WriteDashboard(userid, key, slot[0], slot[1], slot[2], slot[3], tw, twpb)
+
+  #Print response JSON from IoTtweet
+  print(res)
+  time.sleep(3)
